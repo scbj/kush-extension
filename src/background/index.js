336ae3f614b439425e9c5e42392103123dea1d75
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-03-13 08:42:18 +0100

    Persist extension data with local storage

--- src/background/index.js
+++ src/background/index.js
@@ -9,11 +9,13 @@ import { ACCOUNT_AUTHENTICATED } from '@/constants'
 import server from '@/background/server'
 
 function onAuthenticated (payload) {
-  server.authenticate({
-    accessToken: payload.accessToken,
-    refreshToken: payload.refreshToken
-  })
-  server.connect(payload.extension.id)
+  const { accessToken, extension, refreshToken } = payload
+  server.authenticate({ accessToken, refreshToken })
+  server.connect(extension.id)
+
+  localStorage.setItem('accessToken', accessToken)
+  localStorage.setItem('extension', JSON.stringify(extension))
+  localStorage.setItem('refreshToken', refreshToken)
 
   server.on(ACTION_PLAYBACK_TOGGLE_STATUS, () => {
     console.log('🐞: ACTION_PLAYBACK_TOGGLE_STATUS', ACTION_PLAYBACK_TOGGLE_STATUS)
@@ -26,11 +28,17 @@ function onAuthenticated (payload) {
   })
 }
 
+function autoConnect () {
+  const accessToken = localStorage.getItem('accessToken')
+  const extension = JSON.parse(localStorage.getItem('extension'))
+  const refreshToken = localStorage.getItem('refreshToken')
+  if (accessToken && extension) {
+    onAuthenticated({ accessToken, refreshToken, extension })
+  }
+}
+
 message.on(ACCOUNT_AUTHENTICATED, onAuthenticated)
 message.on(EVENT_PLAYBACK_STATUS_CHANGED, payload => server.emit(EVENT_PLAYBACK_STATUS_CHANGED, payload))
 message.on(EVENT_PLAYBACK_TRACK_CHANGED, payload => server.emit(EVENT_PLAYBACK_TRACK_CHANGED, payload))
 
-// const accessToken = localStorage.getItem('accessToken')
-// if (accessToken) {
-//   onAuthenticated({ accessToken })
-// }
+autoConnect()
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
-    <input type="email" name="username" v-model="username">
-    <input type="password" name="password" v-model="password">
-    <button @click="onSubmitClick">Login</button>
-    <p>{{ statusMessage }}</p>
+    <template v-if="alreadyAuthenticated">
+      <ul>
+        <li>ExtensionId: <span>{{ extension.id }}</span></li>
+        <li>IP: <span>{{ extension.ip }}</span></li>
+        <li>OS: <span>{{ extension.os }}</span></li>
+        <li>Navigator: <span>{{ extension.navigator }}</span></li>
+      </ul>
+    </template>
+    <template v-else>
+      <input type="email" name="username" v-model="username">
+      <input type="password" name="password" v-model="password">
+      <button @click="onSubmitClick">Login</button>
+      <p>{{ statusMessage }}</p>
+    </template>
   </div>
 </template>
 
@@ -21,6 +31,15 @@ export default {
       statusMessage: '',
       username: 'jock',
       password: 'd6s45AFpJA92ZAo'
+    }
+  },
+
+  computed: {
+    alreadyAuthenticated () {
+      return !!localStorage.getItem('accessToken')
+    },
+    extension () {
+      return JSON.parse(localStorage.getItem('extension'))
     }
   },
 
@@ -53,5 +72,17 @@ export default {
 html {
   width: 400px;
   height: 400px;
+}
+
+body {
+  font-size: 14px;
+}
+
+li {
+  margin: 1em 0;
+}
+
+li span {
+  font-weight: bold;
 }
 </style>
